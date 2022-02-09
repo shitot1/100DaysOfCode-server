813961ecea941e43d61165c36a2fbf396df266b4
enbarSoco <enbar2232@gmail.com>
2022-02-09 16:12:41 +0200

    fixed bug

--- controllers/questionsController.js
+++ controllers/questionsController.js
@@ -2,7 +2,7 @@ const Question = require('../models/question');
 
 exports.questionsController = {
     getQuestions(req, res) {
-        Question.find({}, { '__v': 0 })
+        Question.find({}, { '__v': 0, '_id': 0 })
             .then(docs => { res.json(docs) })
             .catch(err => res.status(400).send({ "error": `Error getting Data from DB: ${err}` }));
     }
